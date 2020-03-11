fd998f9933cdaecfbb2b60bba7713f5ffd2435a0
Tarly127 <acblou@sapo.pt>
2020-03-11 21:33:48 +0000

    Algumas coisas que faltavam no último commit

--- Codigo/Front-End/src/components/userLatNavBar.jsx
+++ Codigo/Front-End/src/components/userLatNavBar.jsx
@@ -1,35 +1,32 @@
 import React, { Component } from "react";
 
-class userLatNavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <span class="border">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">
-              Active
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Link
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Link
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">
-              Disabled
-            </a>
-          </li>
-        </ul>
-      </span>
-    );
-  }
+function userLatNavBar() {
+  return (
+    <span class="border">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">
+            Active
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Link
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">
+            Disabled
+          </a>
+        </li>
+      </ul>
+    </span>
+  );
 }
 
 export default userLatNavBar;
