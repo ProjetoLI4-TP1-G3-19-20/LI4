84c552af7eb389e4f9e1452a022d059a4f9de124
Pedro <pedromiguel-99@hotmail.com>
2020-03-28 22:29:31 +0000

    Very simple main

---
+++ Codigo/BackEnd/TrabalhoLI4/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Visitas
+{
+    class Visitas
+    {
+        static void Main(string[] args)
+        {
+            HTTPServer s = new HTTPServer("C:", 8080);
+            
+        }
+        
+    }
+}
